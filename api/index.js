e7c60823f4ff31b4827812db35620e7d659cd2fd
anderson camero <cameroanderson22@gmail.com>
2023-03-29 21:47:00 -0500

    se organizan las rutas, se agregan los middlwares

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const routerApi = require('./routes/index')
-const{logErrors, errorHandler, boomerrorHandler}=require('./api/middlewares/error.handler')
+const routerApi = require('../api/routes/index')
+const{logErrors, errorHandler, boomerrorHandler}=require('./middlewares/error.handler')
 const app = express()
 const PORT = process.env.PORT || 8080
 
@@ -25,25 +25,9 @@ app.use(logErrors)
 app.use(boomerrorHandler)
 app.use(errorHandler)
 
-
-app.get('/medidor/:id', (req,res)=>{
-    console.log('serial del medidor: ',req.params.id);
-    res.status(200).send("ping")
-})
-
 app.post('/consulta/:id', (req,res)=>{
     res.status(201).send("inicia peticion de datos del medidor")
     console.log(req);
-})
-
-app.post('/tramas/:id', (req,res)=>{
-    console.log('serial del medidor: ', req.params.id)
-    let today = new Date();
-    let now = today.toLocaleString()
-    let datos = req.body;
-    let variables = JSON.stringify(datos)+"tiempo: "+ now
-    console.log(variables);
-    res.status(202).send("dato recibido")
 })
 
 app.listen(PORT, ()=>{
