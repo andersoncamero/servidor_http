3d7efcb15c00a0e4f6045478220f8918dc82115f
anderson camero <cameroanderson22@gmail.com>
2023-03-29 21:55:11 -0500

    se colocan las rutas para los productos

--- api/routes/users.router.js
+++ api/routes/users.router.js
@@ -1,23 +1,31 @@
 const express = require('express')
 // const {faker} = require('@faker-js/faker')
 const {faker} = require('@faker-js/faker')
-
+const UserService = require('../services/user.sevice')
 
 const router = express.Router()
+const service = new UserService()
 
 
-router.get('/users', (req, res)=>{
+router.get('/', async (req, res)=>{
+    const datos = await service.find()
+    res.json(datos)
+ })
+
+router.get('/medidor/:id', async (req, res)=>{
     const {limit, offset} = req.query
-    if (limit && offset) {
-        res.json({
-            limit,
-            offset
-        })
-    }else{
-        res.send('no hay parametros')
-    }
+    const users = await service.generate(limit, offset)
+    res.json(users)
 })
 
+
+router.post('/tramas/:id', async (req, res)=>{
+    const body  = req.body;
+    const { id } = req.params
+    const  dateToday = new Date().toLocaleString();
+    const medidor = service.create(id, body, dateToday);
+    res.status(201).json({ medidor})
+})
 
 
 module.exports = router
